97914e1273eead89a6494c304864d710f303b1a6
aozumi <68194841+aozumi@users.noreply.github.com>
2020-08-12 03:16:32 +0900

    エラー詳細表示が有効のときはSlimのデフォルトのエラーハンドラを使う

--- classes/exceptions/CustomErrorRenderer.php
+++ classes/exceptions/CustomErrorRenderer.php
@@ -5,6 +5,7 @@ use Throwable;
 use Psr\Container\ContainerInterface;
 use Slim\Interfaces\ErrorRendererInterface;
 use Slim\Views\Twig;
+use Slim\Error\Renderers\HtmlErrorRenderer;
 
 class CustomErrorRenderer implements ErrorRendererInterface
 {
@@ -17,9 +18,19 @@ class CustomErrorRenderer implements ErrorRendererInterface
 
     public function __invoke(Throwable $exception, bool $displayErrorDetails): string
     {
+        if ($displayErrorDetails) {
+            return $this->renderDefaultErrorHtml($exception, $displayErrorDetails);
+        }
+
         $twig = $this->container->get("view");
         $msg = 'もう一度始めから操作してください。';
         $html = $twig->fetch("error.html", ["errorMsg" => $msg]);
         return $html;
+    }
+
+    public function renderDefaultErrorHtml(Throwable $exception, $displayErrorDetails): string
+    {
+        $renderer = new HtmlErrorRenderer();
+        return $renderer($exception, $displayErrorDetails);
     }
 }
