db4d2e1c88a11224a1bae15ed2492cb6905dc4de
aozumi <68194841+aozumi@users.noreply.github.com>
2020-08-11 12:37:14 +0900

    MemberDAOクラスを作成

---
+++ classes/daos/MemberDAO.php
@@ -0,0 +1,40 @@
+<?php
+namespace SocymSlim\MVC\daos;
+
+use PDO;
+use SocymSlim\MVC\entities\Member;
+
+class MemberDAO
+{
+    private $db;  // PDOインスタンス
+
+    public function __construct(PDO $db)
+    {
+        $this->db = $db;
+    }
+
+    private function rowToMember($row): Member {
+        $member = new Member();
+        $member->setId($row['id']);
+        $member->setMbNameLast($row['mb_name_last']);
+        $member->setMbNameFirst($row['mb_name_first']);
+        $member->setMbBirth($row['mb_birth']);
+        $member->setMbType($row['mb_type']);
+        return $member;
+    }
+
+    // 主キーによる検索
+    public function findByPK(int $id): ?Member
+    {
+        $sqlSelect = 'SELECT * FROM members WHERE id = :id';
+        $stmt = $this->db->prepare($sqlSelect);
+        $stmt->bindValue(':id', $id, PDO::PARAM_INT);
+        $result = $stmt->execute();
+        if ($result && $row = $stmt->fetch()) {
+            $member = $this->rowToMember($row);
+            return $member;
+        } else {
+            return null;
+        }
+    }
+}
