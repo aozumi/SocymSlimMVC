3bb199a4c910f50668dc74f1a04805ab26fbed3a
aozumi <68194841+aozumi@users.noreply.github.com>
2020-08-12 19:29:28 +0900

    環境変数DEV_MODEでエラー詳細表示の可否を決める

--- bootstrap.php
+++ bootstrap.php
@@ -1,9 +1,12 @@
 <?php
 use SocymSlim\MVC\exceptions\CustomErrorRenderer;
 
+$DEV_MODE = $_ENV['DEV_MODE'];
+$displayErrorDetails = !!$DEV_MODE;
+
 # ミドルウェアの登録
 $app->addRoutingMiddleware();
-$errorMiddleware = $app->addErrorMiddleware(true, true, true);
+$errorMiddleware = $app->addErrorMiddleware($displayErrorDetails, true, true);
 
 $errorHandler = $errorMiddleware->getDefaultErrorHandler();
 $errorHandler->registerErrorRenderer('text/html', CustomErrorRenderer::class);
